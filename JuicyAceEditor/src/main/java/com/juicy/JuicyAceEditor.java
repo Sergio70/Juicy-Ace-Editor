b584d74836c722255e4cae2ec70d27a9e252d506
darsisit <darsisit@DESKTOP-504M4LJ>
2018-12-10 18:11:19 -0600

    add enum JuicyAceTheme add enum JuicyAceMode

--- JuicyAceEditor/src/main/java/com/juicy/JuicyAceEditor.java
+++ JuicyAceEditor/src/main/java/com/juicy/JuicyAceEditor.java
@@ -1,33 +1,80 @@
 package com.juicy;
 
+import com.juicy.mode.JuicyAceMode;
+import com.juicy.theme.JuicyAceTheme;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasSize;
-import com.vaadin.flow.component.HasText;
+import com.vaadin.flow.component.Synchronize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 
 @SuppressWarnings("serial")
 @Tag("juicy-ace-editor")
 @HtmlImport("bower_components/juicy-ace-editor/juicy-ace-editor.html")
-public class JuicyAceEditor extends Component implements HasSize, HasText {
+public class JuicyAceEditor extends Component implements HasSize {
 
 	public JuicyAceEditor() {
 	}
 
-	public void setMode(String mode) {
+	public void setMode(JuicyAceMode mode) {
 		getElement().setAttribute("mode", "ace/mode/" + mode);
 	}
 
-	public void setTheme(String theme) {
+	public void setTheme(JuicyAceTheme theme) {
 		getElement().setAttribute("theme", "ace/theme/" + theme);
 	}
- 
+
+	public void setFontsize(Integer fontsize) {
+		getElement().setAttribute("fontsize", String.valueOf(fontsize));
+	}
+
+	public void setSofttabs(Boolean softtabs) {
+		getElement().setAttribute("softtabs", String.valueOf(softtabs));
+	}
+
+	public void setTabsize(Integer tabsize) {
+		getElement().setAttribute("tabsize", String.valueOf(tabsize));
+	}
+
+	public void setReadonly(Boolean readonly) {
+		getElement().setAttribute("readonly", String.valueOf(readonly));
+	}
+
+	public void setWrapmode(Boolean wrapmode) {
+		getElement().setAttribute("wrapmode", String.valueOf(wrapmode));
+	}
+
+	public void setMaxLines(Integer maxLines) {
+		getElement().setAttribute("max-lines", String.valueOf(maxLines));
+	}
+
+	public void setMinLines(Integer minLines) {
+		getElement().setAttribute("min-lines", String.valueOf(minLines));
+	}
+
+	public void setShadowStyle(String shadowStyle) {
+		getElement().setAttribute("shadow-style", String.valueOf(shadowStyle));
+	}
+
 	public void clear() {
-		getElement().setText("");
+		getElement().setProperty("value", "");
 	}
 
 	public void setValue(String value) {
-		getElement().setText(value);
+		getElement().setProperty("value", value);
+	}
+
+	@Synchronize(value = { "change" })
+	public String getValue() {
+		return getElement().getProperty("value");
+	}
+
+	public void addValueChangeListener(ValueChangeListener listener) {
+		this.getElement().addEventListener("change", args -> listener.valueChange(this));
+	}
+
+	public static interface ValueChangeListener {
+		public void valueChange(JuicyAceEditor juicyAceEditor);
 	}
 
 }
--- JuicyAceEditor/src/test/java/com/juicy/Demo.java
+++ JuicyAceEditor/src/test/java/com/juicy/Demo.java
@@ -1,5 +1,7 @@
 package com.juicy;
 
+import com.juicy.mode.JuicyAceMode;
+import com.juicy.theme.JuicyAceTheme;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
@@ -9,10 +11,25 @@ public class Demo extends Div {
 	public Demo() {
 
 		JuicyAceEditor juicyAceEditor = new JuicyAceEditor();
-		juicyAceEditor.setTheme("eclipse");
-		juicyAceEditor.setMode("xml");
+		juicyAceEditor.setValue("demo");
+		juicyAceEditor.setTheme(JuicyAceTheme.eclipse);
+		juicyAceEditor.setMode(JuicyAceMode.xml);
 		juicyAceEditor.setWidth("1000px");
 		juicyAceEditor.setHeight("500px");
+		juicyAceEditor.setFontsize(15);
+		juicyAceEditor.setSofttabs(false);
+		juicyAceEditor.setTabsize(15);
+		// juicyAceEditor.setReadonly(true);
+		// juicyAceEditor.setWrapmode(true);
+		// juicyAceEditor.setMaxLines(5);
+		// juicyAceEditor.setMinLines(5);
+		//juicyAceEditor.setShadowStyle("");
+		
+		System.out.println("getValue: " + juicyAceEditor.getValue());
+
+		juicyAceEditor.addValueChangeListener(e -> {
+			System.out.println("ValueChangeListener: " + e.getValue());
+		});
 
 		add(juicyAceEditor);
 	}
